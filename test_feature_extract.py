0fe9ee28801f29a11b41c58240723c8a7a5223cb
Sandra Schumann <sandra.schumann426@gmail.com>
2018-05-22 13:05:55 +0300

    can now find landmarks on face

--- test_feature_extract.py
+++ test_feature_extract.py
@@ -10,14 +10,12 @@ align = openface.AlignDlib("shape_predictor_68_face_landmarks.dat")
 bb = align.getLargestFaceBoundingBox(img)
 landmarks = align.findLandmarks(img,bb)
 
-print (bb.left(),bb.bottom()),(bb.right(),bb.top())
-
 cv2.rectangle(img,(bb.left(),bb.bottom()),(bb.right(),bb.top()),(0,255,0),3)
-cv2.imshow("Lõust",img)
 
 for point in landmarks:
-    cv2.circle(img,point,10,(0,0,255),-1)
+    cv2.circle(img,point,3,(0,0,255),-1)
 
+cv2.imshow("Soust",img)
 
 while 1:
     key = cv2.waitKey(1)
