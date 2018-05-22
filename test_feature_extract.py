ab12ce28582db592016c37042bb5890b73a8b062
Madis Kaspar Nigol <madis1995@gmail.com>
2018-05-22 19:57:42 +0300

    failed xd

--- test_feature_extract.py
+++ test_feature_extract.py
@@ -8,10 +8,13 @@ import numpy as np
 
 import sym_mappings as sym
 
+align = openface.AlignDlib("shape_predictor_68_face_landmarks.dat")
+
 # Read in image data
 def process_image(path):
+    global align
     img = cv2.imread(path)
-    align = openface.AlignDlib("shape_predictor_68_face_landmarks.dat")
+    
     img = align.align(501,img)
     img_n,img_m,_ = img.shape
     bb = align.getLargestFaceBoundingBox(img)
@@ -64,11 +67,14 @@ def write_features_to_file(path,feature_vecs):
 
 def main(args):
     feature_vecs = []
-    for i in range(1,501):
-        print i
-        landmarks, img = process_image("Data_Collection/SCUT-FBP-"+str(i)+".jpg")
-        feature_vecs.append(create_feature_vec(landmarks))
-    
+    for i in range(200,501):
+        try:
+            print i
+            landmarks, img = process_image("Data_Collection/SCUT-FBP-"+str(i)+".jpg")
+            feature_vecs.append(create_feature_vec(landmarks))
+        except Exception,e:
+            print str(i) + " failed xd"
+
     write_features_to_file("featurevectors.txt",feature_vecs)
 
     # Display image
