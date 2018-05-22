cbd0395fa30045b7c3d7f3070f5595d7f23c870c
Sandra Schumann <sandra.schumann426@gmail.com>
2018-05-22 13:01:17 +0300

    detects face rectangle

--- test_feature_extract.py
+++ test_feature_extract.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import dlib
 import openface
 import cv2
@@ -6,3 +9,17 @@ img = cv2.imread("SCUT-FBP-1.jpg")
 align = openface.AlignDlib("shape_predictor_68_face_landmarks.dat")
 bb = align.getLargestFaceBoundingBox(img)
 landmarks = align.findLandmarks(img,bb)
+
+print (bb.left(),bb.bottom()),(bb.right(),bb.top())
+
+cv2.rectangle(img,(bb.left(),bb.bottom()),(bb.right(),bb.top()),(0,255,0),3)
+cv2.imshow("Lõust",img)
+
+for point in landmarks:
+    cv2.circle(img,point,10,(0,0,255),-1)
+
+
+while 1:
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
