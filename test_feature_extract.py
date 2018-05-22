9af1e63c35664c3cca5ee873292a14655290b147
Madis Kaspar Nigol <madis1995@gmail.com>
2018-05-22 13:25:32 +0300

    Min bounding rectangle not working pls help

--- test_feature_extract.py
+++ test_feature_extract.py
@@ -4,6 +4,7 @@
 import dlib
 import openface
 import cv2
+import numpy as np
 
 img = cv2.imread("SCUT-FBP-1.jpg")
 align = openface.AlignDlib("shape_predictor_68_face_landmarks.dat")
@@ -11,7 +12,11 @@ bb = align.getLargestFaceBoundingBox(img)
 landmarks = align.findLandmarks(img,bb)
 
 cv2.rectangle(img,(bb.left(),bb.bottom()),(bb.right(),bb.top()),(0,255,0),3)
+rekt = cv2.minAreaRect(np.array([ (int(x), int(y)) for (x,y) in landmarks ]))
+boks = cv2.BoxPoints(rekt)
+boks = np.int0(boks)
 
+cv2.drawContours(img, [boks], 0, (0,0,255), 2)
 for point in landmarks:
     cv2.circle(img,point,3,(0,0,255),-1)
 
