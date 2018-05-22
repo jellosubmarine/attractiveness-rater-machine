709a8813cee7ab82e29871cbc76834e40b3c7936
Sandra Schumann <sandra.schumann426@gmail.com>
2018-05-22 18:49:53 +0300

    deleted a chunk of test code (SEE PREVIOUS COMMIT IF NEEDED), wrote some crude symmetry evaluation functions

--- test_feature_extract.py
+++ test_feature_extract.py
@@ -7,33 +7,32 @@ import cv2
 import numpy as np
 
 # Read in image data
-img = cv2.imread("SCUT-FBP-1.jpg")
-img_n,img_m,_ = img.shape
+img = cv2.imread("SCUT-FBP-42.jpg")
 align = openface.AlignDlib("shape_predictor_68_face_landmarks.dat")
-img = align.align(500,img)
+img = align.align(501,img)
+img_n,img_m,_ = img.shape
 bb = align.getLargestFaceBoundingBox(img)
 landmarks = align.findLandmarks(img,bb)
-
-#~ # Get some versions of bounding boxes for the face
-#~ cv2.rectangle(img,(bb.left(),bb.bottom()),(bb.right(),bb.top()),(0,255,0),2)
-#~ cv2.line(img,((bb.left()+bb.right())/2,bb.bottom()),((bb.left()+bb.right())/2,bb.top()),(0,255,0),2)
-#~ rekt = cv2.minAreaRect(np.array([ (int(x), int(y)) for (x,y) in landmarks ]))
-#~ boks = cv2.boxPoints(rekt)
-#~ boks = np.int0(boks)
-#~ cv2.drawContours(img, [boks], 0, (0,0,255), 2)
-#~ cv2.line(img,((boks[0][0]+boks[1][0])/2,(boks[0][1]+boks[1][1])/2),((boks[2][0]+boks[3][0])/2,(boks[2][1]+boks[3][1])/2),(0,0,255),2)
 
 # Mark face landmarks on image
 for point in landmarks:
     cv2.circle(img,point,2,(255,0,0),-1)
 
 # Fit linear function to face's line of symmetry
-symmetry_points = [8, 27, 28, 29, 30, 51, 57, 62, 66]
-xs = [ landmarks[i][0] for i in symmetry_points ]
-ys = [ landmarks[i][1] for i in symmetry_points ]
-fit = np.polyfit(ys,xs,1) # note: flipped
-fit_fn = np.poly1d(fit)
-cv2.line(img,(int(fit_fn(0)),0),(int(fit_fn(img_n)),img_n),(255,0,0),2)
+cv2.line(img,(img_m/2,img_n),(img_m/2,0),(255,0,0),2)
+
+# Create symmetry evaluations for facial features
+def eval_symmetry(landmarks, sym_mapping):
+    s = 0
+    for i,j in sym_mapping:
+        a = landmarks[i]
+        b = landmarks[j]
+        s += (abs(a[0]-b[0]) + abs(a[1]-b[1]))
+    return s
+
+# Evaluate whether features in the middle of the face are actually in the middle
+def eval_center(landmarks, middle_list):
+    return sum([ abs(landmarks[i][0]-250) for i in middle_list ])
 
 # Display image
 cv2.imshow("Soust",img)
